a3441340f609d9292de2d2a37b1d4bd715a147cc
Marco <marco.brighi4@studio.unibo.it>
2019-01-29 10:32:47 +0100

    Created random allowed chars generator

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -18,4 +18,5 @@ object Board{
 
     override def isPresent(word: String): Boolean = ???
   }
+
 }
---
+++ src/main/scala/model/RandomGenerator.scala
@@ -0,0 +1,11 @@
+package model
+
+import scala.util.Random
+
+object RandomGenerator extends Function0[Char] {
+
+  val dictionary: String = "abcdefghijklmnopqrstuvwxyz-"
+  val random: Random = new Random()
+
+  override def apply(): Char = dictionary.charAt(random.nextInt(dictionary.length()))
+}
