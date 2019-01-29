75cb09f7e6a054ebb1abb0e4b4316d1182f2ea74
Marco <marco.brighi4@studio.unibo.it>
2019-01-29 11:45:55 +0100

    Implemented the class Board.

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -1,7 +1,7 @@
 package model
 import java.io.InputStream
 
-import prolog.{PredicateBuilder, PrologEngine}
+import prolog._
 import prolog.PredicateBuilder._
 
 trait Board {
@@ -15,7 +15,7 @@ object Board{
 
   def apply(generator: () => Char, size: Int): Board = new BoardImpl(generator, size)
 
-  private class BoardImpl(val generator: () => Char, override val size: Int) extends Board{
+  private class BoardImpl(val generator: () => Char, override val size: Int) extends Board {
 
     val theory: InputStream = getClass().getResourceAsStream("/board.pl")
     val engine: PrologEngine = PrologEngine.loadTheory(theory)
@@ -28,9 +28,20 @@ object Board{
       }
     }
 
-    override def isPresent(word: String): Boolean = ???
+    override def isPresent(word: String): Boolean = {
+      val builder:PredicateBuilder = PredicateBuilder("is_present") += LogicList(word.toLowerCase().toCharArray().map(letter => Constant(letter)))
+      engine.goal(builder.create()).isDefined
+    }
 
-    override def matrix(): Array[Array[Char]] = ???
+    override def matrix(): Array[Array[Char]] = {
+      val matrix: Array[Array[Char]] = Array.ofDim(size, size)
+      for(i <- 0 until size; j <- 0 until size) {
+        val builder:PredicateBuilder = PredicateBuilder("cell") += i += j += 'K'
+        val solution: SolutionSet = engine.goal(builder.create()).get
+        matrix(i)(j) = solution('X').toCharArray()(0)
+      }
+      matrix
+    }
   }
 
 }
