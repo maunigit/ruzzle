4c70b2435df17a996458135f4234080150a770dd
Matteo Mirisola <matteo.mirisola@studio.unibo.it>
2019-02-01 17:31:56 +0100

    Change package

--- src/main/scala/view/DashboardController.scala
+++ src/main/scala/view/DashboardController.scala
@@ -1,17 +1,18 @@
+package view
+
 import javafx.application.Application
 import javafx.event.ActionEvent
 import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.Scene
 import javafx.scene.control._
-import javafx.stage.Stage
 import javafx.scene.layout.{GridPane, VBox}
+import javafx.stage.Stage
 
 class LauchDashboard extends Application {
   override def start(primaryStage: Stage): Unit = {
     //GUI
     val loaderDashboard: FXMLLoader = new FXMLLoader(getClass.getResource("/view/dashboardView.fxml"))
-    val dashboardController = new DashboardController()
-    loaderDashboard.setController(dashboardController)
+    //val dashboardController = new DashboardController()
     val sceneDashboard = new Scene(loaderDashboard.load())
     primaryStage.setTitle("Ruzzle")
     primaryStage.setScene(sceneDashboard)
