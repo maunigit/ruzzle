5d208f0d9f9558a6e9d55339300ba4cd6497ee99
Marco <marco.brighi4@studio.unibo.it>
2019-02-15 15:24:01 +0100

    Created some basic messages for the game.

--- src/main/scala/actors/Messages.scala
+++ src/main/scala/actors/Messages.scala
@@ -1,9 +1,18 @@
 package actors
 
 import akka.actor.Address
+import model.Word
 
-case class NewGameMessage()
+case class NewGameMessage(time: Int)
 
-case class NewDistributedGameMessage()
+case class NewDistributedGameMessage(time: Int)
+
+case class IAmReadyMessage(player: String)
+
+case class StartGame()
 
 case class PartecipateMessage(player: String, gameActor: Address)
+
+case class FoundWordMessage(value: String, tag: String)
+
+case class AppendWordMessage(player: String, word: Word)
--- src/main/scala/actors/Player.scala
+++ src/main/scala/actors/Player.scala
@@ -1,10 +1,20 @@
 package actors
 
 import akka.actor.{Actor, ActorRef}
+import model.WordTag
+import model.WordTag.WordTag
 
 class Player(val name: String, val guiActor: ActorRef) extends Actor {
 
   override def receive: Receive = {
-    case "test" =>
+    case NewGameMessage(time) =>
+    case FoundWordMessage(value, tag) => {
+      val wordType: WordTag = tag match {
+        case "Noun" => WordTag.Noun
+        case "Adjective" => WordTag.Adjective
+        case "Adverb" => WordTag.Adverb
+        case _ => WordTag.Verb
+      }
+    }
   }
 }
