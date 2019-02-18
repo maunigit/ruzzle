fc22557baf3a24819ccb851383a5bcb0d61534a9
Marco <marco.brighi4@studio.unibo.it>
2019-02-18 17:43:02 +0100

    Defined Generator class doc.

--- src/main/scala/model/RandomGenerator.scala
+++ src/main/scala/model/RandomGenerator.scala
@@ -2,6 +2,9 @@ package model
 
 import scala.util.Random
 
+/**
+  * A random generator of chars.
+  */
 object RandomGenerator extends Function0[Char] {
 
   val dictionary: String = "aabcdeefghiijklmnoopqrstuuvwxyz"
