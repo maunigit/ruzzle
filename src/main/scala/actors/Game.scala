d1936e5516572e42a1935fbd9b3e52ccc6b8f0a5
Marco <marco.brighi4@studio.unibo.it>
2019-02-22 11:37:32 +0100

    Defined JoinTheGame into the Game Actor.

--- src/main/scala/actors/Game.scala
+++ src/main/scala/actors/Game.scala
@@ -1,9 +1,22 @@
 package actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef}
+import model.{Board, FakeGenerator, Game}
 
 class Game(val time: Int, val numberOfPlayers: Int) extends Actor {
 
-  override def receive: Receive = ???
+  var players: List[(String, ActorRef)] = List()
+  var game: Option[model.Game] = Option.empty
+
+  override def receive: Receive = {
+    case JoinTheGame(player) =>
+      players = players :+ (player, sender())
+      sender() ! YouAreIn()
+      if(players.length == numberOfPlayers) {
+        val board: Board = Board(FakeGenerator, 10)
+        game = Option(Game(players.map{case (user, _) => user}, board, time))
+        players.map{case (_, ref) => ref}.foreach(ref => ref ! Start(board.matrix(), time))
+      }
+  }
 
 }
