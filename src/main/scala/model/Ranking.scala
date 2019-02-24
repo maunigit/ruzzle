31f76698febbcf8b1541a975e8e56c13c4bc788d
Matteo Mirisola <matteo.mirisola@studio.unibo.it>
2019-02-24 20:23:49 +0100

    Change Ranking into object, add checkfile and serialization

--- src/main/scala/model/Ranking.scala
+++ src/main/scala/model/Ranking.scala
@@ -1,8 +1,29 @@
 package model
 
+import java.io._
+
 @SerialVersionUID(100L)
-class Ranking extends Serializable {
+object Ranking extends Serializable {
   var rankList: List[(String, Int)] = List(("Mario", 150), ("Luigi", 25))
 
+  def checkFile(file: File): Unit = {
+    if (!file.exists() || file.isDirectory()) {
+      file.createNewFile()
+    }
+  }
+
   def getItemList(): List[(String, Int)] = rankList
+
+  def read(fileName: String): Unit = {
+    val ois = new ObjectInputStream(new FileInputStream(fileName))
+    val rank = ois.readObject.asInstanceOf[Ranking.type]
+    ois.close
+  }
+
+  def write(fileName: String): Unit = {
+    val oos = new ObjectOutputStream(new FileOutputStream(fileName))
+    oos.writeObject(Ranking)
+    oos.close
+  }
+
 }
