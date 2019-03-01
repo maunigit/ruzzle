e5b34dec98b2f459a50a24106cac86ef61c97c2a
Matteo Mirisola <matteo.mirisola@studio.unibo.it>
2019-03-01 11:29:42 +0100

    Add new multiplayer game, join existing game and vowel, consonat, synonymous points dialog

--- src/main/scala/view/DashboardController.scala
+++ src/main/scala/view/DashboardController.scala
@@ -34,7 +34,13 @@ class LauchDashboard extends Application {
 class DashboardController extends Initializable {
 
   @FXML
+  var existGameMenuItem: MenuItem = _
+
+  @FXML
   var matchMenuItem: MenuItem = _
+
+  @FXML
+  var matchMultiplayerMenuItem: MenuItem = _
 
   @FXML
   var pointsMenuItem: MenuItem = _
@@ -85,40 +91,66 @@ class DashboardController extends Initializable {
     val adjectiveLabel: Label = new Label("Adjective Points: ")
     val adverbLabel: Label = new Label("Adverb Points: ")
     val verbLabel: Label = new Label("Verb Points: ")
+    val vowelLabel: Label = new Label("Vowel Points: ")
+    val consonantLabel: Label = new Label("Consonant Points: ")
+    val synonymousLabel: Label = new Label("Synonymous Points: ")
     var nounSlider: Slider = new Slider(1, 9, 3)
     var adjectiveSlider: Slider = new Slider(1, 9, 4)
     var adverbSlider: Slider = new Slider(1, 9, 4)
     var verbSlider: Slider = new Slider(1, 9, 3)
+    var vowelSlider: Slider = new Slider(1, 9, 2)
+    var consonantSlider: Slider = new Slider(1, 9, 1)
+    var synonymousSlider: Slider = new Slider(1, 9, 1)
     var nounValue = new Label(nounSlider.getValue().toInt.toString())
     var adjectiveValue = new Label(adjectiveSlider.getValue().toInt.toString())
     var adverbValue = new Label(adverbSlider.getValue().toInt.toString())
     var verbValue = new Label(verbSlider.getValue().toInt.toString())
-
+    var vowelValue = new Label(vowelSlider.getValue().toInt.toString())
+    var consonantValue = new Label(consonantSlider.getValue().toInt.toString())
+    var synonymousValue = new Label(synonymousSlider.getValue().toInt.toString())
     val grid: GridPane = new GridPane()
     GridPane.setConstraints(nounLabel, 0, 0)
     GridPane.setConstraints(adjectiveLabel, 0, 1)
     GridPane.setConstraints(adverbLabel, 0, 2)
     GridPane.setConstraints(verbLabel, 0, 3)
+    GridPane.setConstraints(vowelLabel, 0, 4)
+    GridPane.setConstraints(consonantLabel, 0, 5)
+    GridPane.setConstraints(synonymousLabel, 0, 6)
     grid.getChildren().add(nounLabel)
     grid.getChildren().add(adjectiveLabel)
     grid.getChildren().add(adverbLabel)
     grid.getChildren().add(verbLabel)
+    grid.getChildren().add(vowelLabel)
+    grid.getChildren().add(consonantLabel)
+    grid.getChildren().add(synonymousLabel)
     GridPane.setConstraints(nounSlider, 1, 0)
     GridPane.setConstraints(adjectiveSlider, 1, 1)
     GridPane.setConstraints(adverbSlider, 1, 2)
     GridPane.setConstraints(verbSlider, 1, 3)
+    GridPane.setConstraints(vowelSlider, 1, 4)
+    GridPane.setConstraints(consonantSlider, 1, 5)
+    GridPane.setConstraints(synonymousSlider, 1, 6)
     grid.getChildren.add(nounSlider)
     grid.getChildren.add(adjectiveSlider)
     grid.getChildren.add(adverbSlider)
     grid.getChildren.add(verbSlider)
+    grid.getChildren.add(vowelSlider)
+    grid.getChildren.add(consonantSlider)
+    grid.getChildren.add(synonymousSlider)
     GridPane.setConstraints(nounValue, 2, 0)
     GridPane.setConstraints(adjectiveValue, 2, 1)
     GridPane.setConstraints(adverbValue, 2, 2)
     GridPane.setConstraints(verbValue, 2, 3)
+    GridPane.setConstraints(vowelValue, 2, 4)
+    GridPane.setConstraints(consonantValue, 2, 5)
+    GridPane.setConstraints(synonymousValue, 2, 6)
     grid.getChildren().add(nounValue)
     grid.getChildren().add(adjectiveValue)
     grid.getChildren().add(adverbValue)
     grid.getChildren().add(verbValue)
+    grid.getChildren().add(vowelValue)
+    grid.getChildren().add(consonantValue)
+    grid.getChildren().add(synonymousValue)
     nounSlider.valueProperty().addListener(new ChangeListener[Number] {
       override def changed(observable: ObservableValue[_ <: Number], oldValue: Number, newValue: Number): Unit = {
         ScoreManager.nounPoints = newValue.intValue()
@@ -143,8 +175,30 @@ class DashboardController extends Initializable {
         verbValue.setText(newValue.intValue().toString())
       }
     })
+    vowelSlider.valueProperty().addListener(new ChangeListener[Number] {
+      override def changed(observable: ObservableValue[_ <: Number], oldValue: Number, newValue: Number): Unit = {
+        ScoreManager.vowelPoints = newValue.intValue()
+        vowelValue.setText(newValue.intValue().toString())
+      }
+    })
+    consonantSlider.valueProperty().addListener(new ChangeListener[Number] {
+      override def changed(observable: ObservableValue[_ <: Number], oldValue: Number, newValue: Number): Unit = {
+        ScoreManager.consonantPoints = newValue.intValue()
+        consonantValue.setText(newValue.intValue().toString())
+      }
+    })
+    synonymousSlider.valueProperty().addListener(new ChangeListener[Number] {
+      override def changed(observable: ObservableValue[_ <: Number], oldValue: Number, newValue: Number): Unit = {
+        ScoreManager.synonymousPoints = newValue.intValue()
+        synonymousValue.setText(newValue.intValue().toString())
+      }
+    })
     alert.getDialogPane().setContent(grid)
     alert.showAndWait()
+  }
+
+  @FXML def joinExistGame(event: ActionEvent): Unit = {
+
   }
 
   @FXML def newGameMatch(event: ActionEvent): Unit = {
@@ -154,6 +208,10 @@ class DashboardController extends Initializable {
     } else {
       // crea l'agente giocatore
     }
+  }
+
+  @FXML def newGameMatchMultiplayer(event: ActionEvent): Unit = {
+
   }
 
   @FXML def searchWord(event: ActionEvent): Unit = {
