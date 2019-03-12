a17e0137fc245f5e54e90f232ad7525244f9d74d
Matteo Mirisola <matteo.mirisola@studio.unibo.it>
2019-03-12 18:23:10 +0100

    Add docs and reformatting rank

--- src/main/java/view/Rank.java
+++ src/main/java/view/Rank.java
@@ -3,6 +3,9 @@ package view;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * The Rank class that allows to display the ranking data appropriately.
+ */
 public class Rank {
     private final SimpleStringProperty username;
     private final SimpleIntegerProperty points;
@@ -12,10 +15,20 @@ public class Rank {
         this.points = new SimpleIntegerProperty(point);
     }
 
+    /**
+     * Get the username of the person.
+     *
+     * @return
+     */
     public String getUsername() {
         return username.get();
     }
 
+    /**
+     * Get the points of the person.
+     *
+     * @return
+     */
     public Integer getPoints() {
         return points.get();
     }
